c073bab33ad16c4ea4a90b30dcc72687f1277abe
Luis Güette <guetteluis@gmail.com>
2020-05-01 23:12:22 -0400

    rename  firebase_auth to firebase_authentication

--- lib/User/repository/auth_repository.dart
+++ lib/User/repository/auth_repository.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:places/User/repository/firebase_auth.dart';
+import 'package:places/User/repository/firebase_authentication.dart';
 
 class AuthRepository {
 

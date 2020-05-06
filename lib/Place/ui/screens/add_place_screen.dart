c2e2370cf1bb3574d9cbfa06b45d8c75f5553dbc
Luis Güette <guetteluis@gmail.com>
2020-05-05 21:39:44 -0400

    add inputs

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:places/widgets/gradient_background.dart';
 import 'package:places/widgets/header_title.dart';
+import 'package:places/widgets/text_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
 
@@ -18,6 +19,9 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreen extends State<AddPlaceScreen> {
+
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +53,29 @@ class _AddPlaceScreen extends State<AddPlaceScreen> {
                 )
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: <Widget>[
+                Container(),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                      hintText: "Title",
+                      inputType: null,
+                      maxLines: 1,
+                      controller: titleController
+                  ),
+                ),
+                TextInput(
+                  hintText: "Description",
+                  inputType: TextInputType.multiline,
+                  maxLines: 4,
+                  controller: descriptionController
+                ),
+              ],
+            ),
           )
         ],
       ),
